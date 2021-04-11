0b3eb9ba40bf55ff14e8d8badcec2744c4ed2f6e
ComputingandCoding <computingandcoding@gmail.com>
2021-04-11 13:11:55 -0400

    Updates

---
+++ precache-manifest.19553432a00e3abb74e8ed9bcb8b09fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3806971bc2008d26100152cc221852a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f89110b4dd049b7700a8",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "33329c945ed82ac5fedd",
+    "url": "/static/css/main.e82a989e.chunk.css"
+  },
+  {
+    "revision": "f89110b4dd049b7700a8",
+    "url": "/static/js/2.199f516d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.199f516d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33329c945ed82ac5fedd",
+    "url": "/static/js/main.b9581088.chunk.js"
+  },
+  {
+    "revision": "96cc80857a0d174cd51e",
+    "url": "/static/js/runtime-main.3d29c6cb.js"
+  }
+]);
